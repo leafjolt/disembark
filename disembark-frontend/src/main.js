bc57b1ae01dfcb593a9a6648c8697d31cd6a4ec7
Shaan Desai <leafjolt@gmail.com>
2026-05-08 19:36:02 +0200

    Fixed loading of stores in main frontend file

--- disembark-frontend/src/main.js
+++ disembark-frontend/src/main.js
@@ -3,9 +3,14 @@ import { createPinia } from 'pinia'
 import router from './router'
 import './style.css'
 import App from './App.vue'
+import { useAuthStore } from './stores/auth'
+import { useTripsStore } from './stores/trips'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
 app.mount('#app')
+
+useAuthStore();
+useTripsStore();
