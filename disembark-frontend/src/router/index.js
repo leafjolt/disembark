7ec6ac5ce5c157d78415898b6bfc37caf7f0e9b2
Shaan Desai <leafjolt@gmail.com>
2026-05-08 19:44:26 +0200

    Attempted web history fix with hashes

--- disembark-frontend/src/router/index.js
+++ disembark-frontend/src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory, createWebHashHistory } from 'vue-router'
 import { useAuthStore } from '../stores/auth'
 
 const routes = [
@@ -12,7 +12,8 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory('/disembark/'),
+ // history: createWebHistory('/disembark/'),
+  history: createWebHashHistory(),
   routes
 })
 
