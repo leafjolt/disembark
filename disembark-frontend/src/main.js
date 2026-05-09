af27a8a85c171dfbcd0b54bd1cd852d7db95e4b4
Shaan Desai <leafjolt@gmail.com>
2026-05-09 16:42:56 +0200

    Trying to fix GitHub Pages issue

--- disembark-frontend/src/main.js
+++ disembark-frontend/src/main.js
@@ -6,9 +6,11 @@ import App from './App.vue'
 import { useAuthStore } from './stores/auth'
 import { useTripsStore } from './stores/trips'
 
+const pinia = createPinia();
+
 const app = createApp(App)
 
-app.use(createPinia())
+app.use(pinia)
 app.use(router)
 app.mount('#app')
 
